913ba0e06565db86b90f44511522bfc28696f898
Aspodel <dattran2309@gmail.com>
2022-07-20 23:28:33 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BlobQuickstartV12
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Azure Blob Storage v12 - .NET quickstart sample\n");
+
+            string resultURL = await UploadFile();
+            Console.WriteLine("{0}", resultURL);
+        }
+
+        static async Task<string> UploadFile()
+        {
+
+            string? connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            // Create the container and return a container client object
+            // BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync("containername");
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("containername");
+
+
+            string localPath = "./data/";
+            string fileName = "quickstart" + Guid.NewGuid().ToString() + ".txt";
+            string localFilePath = Path.Combine(localPath, fileName);
+
+            // Write text to the file
+            await File.WriteAllTextAsync(localFilePath, "Hello, World!");
+
+            // Get a reference to a blob
+            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+            // Upload data from the local file
+            await blobClient.UploadAsync(localFilePath, true);
+
+            return blobClient.Uri.ToString();
+        }
+    }
+}
